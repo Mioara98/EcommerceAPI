04674d9e769885dc11b207bd78966377a5136a00
Mioara98 <sterie_mioara@yahoo.com>
2025-01-06 19:24:23 +0200

    Create
    fs

--- Backend/Backend.Domain/Users/Address.cs
+++ Backend/Backend.Domain/Users/Address.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Backend.Domain.Users
 {
-    internal class Address
+    public class Address
     {
+        public string Id {  get; set; }
+        public int PostalCode { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Phone { get; set; }
+        public int? ApartmentNumber { get; set; }
+        public int? BlocNumber { get; set; }
+
     }
 }
--- Backend/Backend.Domain/Users/User.cs
+++ Backend/Backend.Domain/Users/User.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Backend.Domain.Users
 {
-    internal class User
+    public class User
     {
+        public string Email {  get; set; }
+        public string Password { get; set; }
+        public string Id { get; set; }
+        public string Image { get; set; }
+        public  List<Address>  Addresses { get; set; }
+        
+        
     }
 }
