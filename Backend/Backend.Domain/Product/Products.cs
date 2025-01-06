82596141673da45c25c31bfa812bf728aa123cd1
Mioara98 <sterie_mioara@yahoo.com>
2025-01-06 19:36:20 +0200

    create Products
    Am creat clasa de produse

---
+++ Backend/Backend.Domain/Product/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Product
+{
+    public class Products
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int YearofManufacture {  get; set; }
+        
+    }
+}
